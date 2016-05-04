4282a0291e755094216346a8e83089c87d7d8174
h4ck3r <h4ck3r@h4ck3r-PC>
2016-05-04 10:10:27 -0300

    Classe de conexao com a tabela medidas2 criada

---
+++ src/bancodedados/ConexaoMedidas2.java
@@ -0,0 +1,81 @@
+package bancodedados;
+
+import hide.Medidas2;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ConexaoMedidas2 {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String URL_BANCO = "jdbc:mysql://localhost/academia";
+    static final String USUARIO = "root";
+    static final String SENHA_BANCO = "2104";
+    private final Medidas2 medidas = new Medidas2();
+    
+    
+    
+    private Date dataFormt(String data) throws ParseException{
+        data = dataFormatSqlInver(data);
+        DateFormat f = DateFormat.getDateInstance();
+        return f.parse(data);
+    }
+    
+    private String dataFormatSql(Date data){
+        Calendar a = Calendar.getInstance();
+        a.set(Calendar.DAY_OF_MONTH, data.getDate());
+        a.set(Calendar.MONTH, data.getMonth());
+        a.set(Calendar.YEAR, data.getYear()+1900); 
+        
+        return String.format("%d-%d-%d", a.get(Calendar.YEAR), a.get(Calendar.MONTH)+1,
+                a.get(Calendar.DAY_OF_MONTH));
+    }
+    
+    private String dataFormatSqlInver(String dataSql){
+        String arrayData[] = dataSql.split("-");               
+        return String.format("%s/%s/%s", arrayData[2], arrayData[1], arrayData[0]);
+    }  
+    
+    public void addMedidas(Medidas2 medidas){
+        Connection connection = null; //Gerencia a conexão
+        Statement statement = null; //Instrução de consulta
+        
+        try{
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(URL_BANCO, USUARIO, SENHA_BANCO);
+            statement = connection.createStatement();
+            String comando = null;
+            
+            
+            comando = String.format("INSERT INTO medidas2 (idCliente, peso, "
+                    + "altura, imc, dataMedicao) VALUES (%d, %s, %s, %s ,\'%s\')", 
+                    medidas.getIdCliente(), medidas.getPeso(), medidas.getAltura(),
+                    medidas.getImc(), dataFormatSql(medidas.getDataMedicao()));
+            
+            System.out.println(comando);
+            
+            int resultado = statement.executeUpdate(comando);
+            System.out.println("retorno = "+resultado);
+
+        }catch(SQLException e){
+            System.out.println("ERRO SQLException");
+            e.printStackTrace();            
+            System.exit(1);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+            System.exit(1);
+        }finally{
+            try{
+                statement.close();
+                connection.close();
+            }catch(Exception e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+}
